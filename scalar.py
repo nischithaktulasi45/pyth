c8aa2585c00682745d22ad6265e108f0a28547e6
sidduhemanth <ssiddu8619@gmail.com>
2022-11-14 14:41:52 +0530

    first change

---
+++ scalar.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 14 14:15:51 2022
+
+@author: sptcse18
+"""
+
+number_of_units=50
+mass_of_sun=1.989e30
+length_great_wall_of_china=21196
+print(number_of_units)
+print(mass_of_sun)
+print(length_great_wall_of_china)
